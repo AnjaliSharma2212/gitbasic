af2b254dfa6435307abd2e1380e173922adf30a5
Anjali <anjalivce19@gmail.com>
2024-11-18 14:41:13 +0530

    add : index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// Find Even Numbers in an Array
+function findEvenNumbers(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
+
+// Find Odd Numbers in an Array
+function findOddNumbers(arr) {
+    return arr.filter(num => num % 2 !== 0);
+}
+
+// Print a Pattern
+function printPattern(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('* '.repeat(i));
+    }
+}
+
+// Test the functions
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("Even Numbers:", findEvenNumbers(numbers));
+console.log("Odd Numbers:", findOddNumbers(numbers));
+
+console.log("Pattern:");
+printPattern(5);
